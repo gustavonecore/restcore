c2edf77e0937fa1d16792e24d5778178d3cf0651
Gustavo Delgado <gustavod@digintent.com>
2016-03-09 17:31:45 -0300

    initial commit

---
+++ restcore.js
@@ -0,0 +1,48 @@
+(function($)
+{
+	$.restCore = function(method, url, query, next, options)
+	{
+		if (!method)
+		{
+			throw Error('You must define the HTTP method');
+		}
+		if (!url)
+		{
+			throw Error('You must define the url endpoint');
+		}
+		if (!next || !(next instanceof Function))
+		{
+			throw Error('You must define the [next] callback');
+		}
+
+		$.ajax({
+			data: query,
+			beforeSend: function(xhr)
+			{
+				if (options && options.headers)
+				{
+					for (var i in headers)
+					{
+						xhr.setRequestHeader(headers[i].header, headers[i].value);
+					}
+				}
+			},
+			url: url,
+			crossDomain: true,
+			dataType: "json",
+			type: method.toUpperCase(),
+			success: function (response)
+			{
+				next(null, response);
+			},
+			error: function(xhr,status,error)
+			{
+				next({
+					error: error,
+					xhr: xhr,
+					status: status
+				}, null);
+			}
+		});
+	};
+}(jQuery));
